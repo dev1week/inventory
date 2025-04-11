1753a899e8d8040450720169ef16a8e0619999f8
dev1week <rondo2860@gmail.com>
2025-04-11 17:01:28 +0900

    chore: initialize prj setting

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "fastcampus-test-inventory"
+
+plugins {
+    // See https://jmfayard.github.io/refreshVersions
+    id("de.fayard.refreshVersions") version "0.60.5"
+}
+
+include(
+    "practices:simple",
+    "practices:inventory"
+)
